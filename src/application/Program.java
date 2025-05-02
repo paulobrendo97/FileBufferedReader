de93b6967d487687039ddfcb8b888a76fdc95780
Paulo Brendo <paulobrendo016@icloud.com>
2025-05-02 20:28:59 -0300

    atualização

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,13 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		String path = "C:\\Users\\Suke\\eclipse-workspace\\TestFile\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br  = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {		
 			String line = br.readLine();
 			
 			while (line != null) {
@@ -26,20 +21,6 @@ public class Program {
 		catch (IOException e){
 			System.out.println("Error " + e.getMessage());
 		}
-		finally {
-			try {
-				if (br != null) {
-					br.close(); 
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch(IOException e) {
-				e.printStackTrace(); //caso dê erro ao fechar os streammes, vai mostrar onde foi o erro
-			}
-		}
-
 	}
 
 }
